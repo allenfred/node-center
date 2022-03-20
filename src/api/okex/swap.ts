a63b60cb97ca25b902e03600b476e495193a9b4a
Allen.F.Cheng <allen.f.cheng@pwc.com>
2022-03-20 11:23:33 +0800

    update

--- src/api/okex/swap.ts
+++ src/api/okex/swap.ts
@@ -10,8 +10,12 @@ export async function initOkxInsts(): Promise<Instrument[]> {
   let instruments: Array<Instrument> = await getOkxSwapInsts();
 
   // BTC合约及其他USDT本位合约
-  instruments = instruments.filter((i) => i.instrument_id.endsWith('USDT-SWAP'));
-  logger.info(`Okx[永续合约] - 获取公共合约全量信息成功，共: ${instruments.length} 条 ...`);
+  instruments = instruments.filter((i) =>
+    i.instrument_id.endsWith('USDT-SWAP'),
+  );
+  logger.info(
+    `Okx[永续合约] - 获取公共合约全量信息成功，共: ${instruments.length} 条 ...`,
+  );
 
   //更新永续合约信息
   await InstrumentInfoDao.upsert(instruments);
@@ -20,13 +24,20 @@ export async function initOkxInsts(): Promise<Instrument[]> {
   return instruments;
 }
 
-export async function getOkxHistoryKlines(instruments: Instrument[], days?: number): Promise<void> {
+export async function getOkxHistoryKlines(
+  instruments: Instrument[],
+  days?: number,
+): Promise<void> {
   //获取所有时间粒度请求参数 如[60/180/300 900/1800/3600/7200/14400/21600/43200/86400]
   return bluebird.map(
     instruments,
     async (instrument: Instrument) => {
-      return await getLatestKlines({ exchange: Exchange.Okex, instId: instrument.instrument_id, count: 500 });
+      return await getLatestKlines({
+        exchange: Exchange.Okex,
+        instId: instrument.instrument_id,
+        count: 300,
+      });
     },
-    { concurrency: 2 }
+    { concurrency: 2 },
   );
 }
