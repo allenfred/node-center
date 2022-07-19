5c5d581c7b64e58a64be7617148ad99b5025a378
Allen.F.Cheng <allen.f.cheng@pwc.com>
2022-07-19 11:23:23 +0800

    update

--- dist/dao/instrumentInfo.js
+++ dist/dao/instrumentInfo.js
@@ -12,6 +12,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.InstrumentInfoDao = void 0;
 const bluebird = require("bluebird");
 const models_1 = require("../database/models");
+const _ = require("lodash");
 function upsert(instruments) {
     return __awaiter(this, void 0, void 0, function* () {
         return bluebird.map(instruments, (item) => __awaiter(this, void 0, void 0, function* () {
@@ -48,42 +49,12 @@ function deleteByIds(instIds, exchange) {
 function findAll() {
     return __awaiter(this, void 0, void 0, function* () {
         // 获取所有合约信息
-        const insts = yield models_1.InstrumentInfo.aggregate([
-            { $sort: { exchange: 1 } },
-            {
-                $group: {
-                    _id: '$base_currency',
-                    base_currency: { $first: '$base_currency' },
-                    quote_currency: { $first: '$quote_currency' },
-                    exchange: { $first: '$exchange' },
-                    volume_24h: { $first: '$volume_24h' },
-                    instrument_id: { $first: '$instrument_id' },
-                },
-            },
-            { $sort: { volume_24h: -1 } },
-            {
-                $project: {
-                    instrument_id: '$instrument_id',
-                    base_currency: '$base_currency',
-                    quote_currency: '$quote_currency',
-                    exchange: '$exchange',
-                    volume_24h: '$volume_24h',
-                },
-            },
-        ]);
-        insts.forEach((i) => {
-            console.log(i.exchange, i.base_currency);
-        });
+        let insts = yield models_1.InstrumentInfo.find().exec();
+        insts = _.orderBy(insts, ['volume_24h'], ['desc']);
+        insts = _.uniqBy(insts, 'base_currency');
         return insts;
     });
 }
-// async function findAll() {
-//   // 获取所有合约信息
-//   let insts = await InstrumentInfo.find().exec();
-//   insts = _.orderBy(insts, ['volume_24h'], ['desc']);
-//   insts = _.uniqBy(insts, 'base_currency');
-//   return insts;
-// }
 const InstrumentInfoDao = {
     upsert,
     find,
